0d0174708fc1cc39146e2bd534263e1134a20a5e
Shijakoof <sijakov.filip@gmail.com>
2022-06-03 08:16:47 +0200

    'Final'

--- Blog/forms.py
+++ Blog/forms.py
@@ -3,12 +3,26 @@ from .models import Blog, BlockedUsers
 
 
 class BlogForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(BlogForm, self).__init__(*args, **kwargs)
+        for field in self.visible_fields():
+            print(field)
+            field.field.widget.attrs["class"] = "form-control"
+
     class Meta:
         model = Blog
         exclude = ('author', 'date_created', 'last_changed')
 
 
 class BlockedUsersForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(BlockedUsersForm, self).__init__(*args, **kwargs)
+        for field in self.visible_fields():
+            print(field)
+            field.field.widget.attrs["class"] = "form-control"
+
     class Meta:
         model = BlockedUsers
         exclude = ('from_user',)
